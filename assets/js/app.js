780b552c3c168a303d51fe949d5e1c8cdb98a20a
Taras Mavrovich <taras.mavrovich1998@gmail.com>
2022-07-05 20:25:04 +0300

    teras-mavr-cv-24-37-47: fix some bugs and add new files index_en/app.js

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+document.getElementById("switchlang").addEventListener("click", function() {
+  const localization = document.getElementById("switchlang");
+  localization?.checked ? window.location.href = 'http://127.0.0.1:5500/index_en.html': window.location.href = 'http://127.0.0.1:5500/index.html'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
