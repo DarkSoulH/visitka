98a04ae95c5792bee56227def7b1174c2b4fa0e8
Taras Mavrovich <taras.mavrovich1998@gmail.com>
2022-07-06 19:18:10 +0300

    teras-mavr-cv-47: fix tag for toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,4 @@
 document.getElementById("switchlang").addEventListener("click", function() {
   const localization = document.getElementById("switchlang");
-  localization?.checked ? window.location.href = 'http://127.0.0.1:5500/index_en.html': window.location.href = 'http://127.0.0.1:5500/index.html'
+  localization?.checked ? window.location.href = 'index_en.html': window.location.href = 'index.html'
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
