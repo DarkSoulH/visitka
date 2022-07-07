1d00c37f2347cdcf731c77a7ec8e61bb3faf6384
Taras Mavrovich <taras.mavrovich1998@gmail.com>
2022-07-07 20:20:26 +0300

    teras-mavr-cv-47:remove tag span and add tag label for my switch too both html and fix style for label

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+"use strict"
 document.getElementById("switchlang").addEventListener("click", function(swlang) {
   const localization = document.getElementById("switchlang");
   localization?.checked ? window.location.href = 'index_en.html': window.location.href = 'index.html'
