a59fb041a251f972a3682c2a4f9bbde9aaad4d84
Taras Mavrovich <taras.mavrovich1998@gmail.com>
2022-07-07 20:00:36 +0300

    teras-mavr-cv-47:added name for function(swlang)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-document.getElementById("switchlang").addEventListener("click", function() {
+document.getElementById("switchlang").addEventListener("click", function(swlang) {
   const localization = document.getElementById("switchlang");
   localization?.checked ? window.location.href = 'index_en.html': window.location.href = 'index.html'
 })
