285570504b6cba89e34d9589ba1e6bc2f62824d8
Taras Mavrovich <taras.mavrovich1998@gmail.com>
2022-07-10 11:44:58 +0300

    teras-mavr-cv-47: fix my checkbox inside hendler vie event

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,12 @@
 "use strict"
-  const localization = document.getElementById("switchlang");
-  function handleClickLang() {
-    localization?.checked ? window.location.href = 'index_en.html': window.location.href = 'index.html'
-  }
-  localization.addEventListener('click', handleClickLang)
+
+const localization = document.getElementById("switchlang");
+
+function handler(event) {
+  event.target.checked ?
+    window.location.href = 'index_en.html':
+    window.location.href = 'index.html'
+}
+
+localization.addEventListener('click', handler)
+
